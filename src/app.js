8f58a37c3855cc850766efce36b45d76e3541f6a
isabellapresto <isabella.presto@gmail.com>
2023-02-28 12:19:00 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+
+function toggleButton() {
+   navList.classList.toggle('show')
+}
+
+
+hamburgerButton.addEventListener('click', toggleButton)
